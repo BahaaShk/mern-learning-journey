71a61b85a4647bce2e64e2d40d17ebf3c5db2c5c
BahaaShk <bahaa.shk@gmail.com>
2026-01-08 22:44:18 +0200

    followed best practices by code splitting

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,15 @@
+export const getAllNotes = (req, res) => {
+  res.status(200).send("you successfully fetched data");
+};
+
+export const createNote = (req, res) => {
+  res.status(201).json({ message: "post is created successfully" });
+};
+
+export const updateNote = (req, res) => {
+  res.status(200).json({ message: "post is updated successfully" });
+};
+
+export const deleteNote = (req, res) => {
+  res.status(200).json({ message: "post is deleted successfully" });
+};
