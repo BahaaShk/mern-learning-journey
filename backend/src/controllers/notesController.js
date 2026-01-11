155531c87a08c9fdabd5b2a7bb642e63e13f7d64
BahaaShk <bahaa.shk@gmail.com>
2026-01-11 23:32:22 +0200

    enhance notes controller with CRUD operations and add sorting for notes retrieval

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export const getAllNotes = async (req, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({createdAt:-1});
 
     res.status(200).json(notes);
   } catch (error) {
@@ -10,24 +10,55 @@ export const getAllNotes = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+export const getNoteById = async (req, res) => {
+  try {
+const note = await Note.findById(req.params.id)
+if(!note) return res.status(404).json({message:"Note not found"})
+  res.status(200).json(note)
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const createNote = async (req, res) => {
-try {
-  const {title,content} = req.body
-  const note = new Note({title, content})
+  try {
+    const { title, content } = req.body;
+    const note = new Note({ title, content });
 
-const savedNote = await newNote.save()
-  res.status(201).json(savedNote)
-} catch (error) {
-      console.error("Error in createNote controller", error);
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.error("Error in createNote controller", error);
     res.status(500).json({ message: "Internal server error" });
-}
+  }
 };
 
 export const updateNote = async (req, res) => {
-  res.status(200).json({ message: "post is updated successfully" });
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, {
+      title,
+      content,
+    },
+  {new:true});
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json({ message: "Note is updated" });
+  } catch (error) {
+    console.error("Error in updateNote controller", error);
+    res.status(500).json({ message: "internal server error" });
+  }
 };
 
 export const deleteNote = async (req, res) => {
-  res.status(200).json({ message: "post is deleted successfully" });
+    try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json({ message: "Note is deleted" });
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({ message: "internal server error" });
+  }
 };
