ff91d9e8a6b2dde937079a893e650200c5655214
BahaaShk <bahaa.shk@gmail.com>
2026-01-22 21:03:37 +0200

    deployment preparation

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,27 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from './config/db.js'
 import dotenv from 'dotenv'
+import path from'path'
 import cors from 'cors'
 
 dotenv.config();
-const PORT = process.env.PORT || 5001
 const app = express();
-
+const PORT = process.env.PORT || 5001
+const _dirname = path.resolve()
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors())
+}
 app.use(express.json());
-app.use(cors())
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(_dirname, "../frontend/dist")))
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(_dirname, "frontend", 'dist', "index.html"))
+})
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server is on port 5001");
